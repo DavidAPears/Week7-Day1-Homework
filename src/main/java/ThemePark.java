ebaa2d5a97a8d02a0e1c03c9115b07c8c1186d78
David A Pears <davidapears@gmail.com>
2018-07-02 22:40:28 +0100

    ThemePark classes/tests added buit can't get Polymorphism part of MVP top work. Giving up for the evening. 22.40h

---
+++ src/main/java/ThemePark.java
@@ -0,0 +1,23 @@
+import java.util.ArrayList;
+
+public class ThemePark {
+
+//    ArrayList<Attraction> attraction;
+//    ArrayList<Stall> stalls;
+//    ArrayList<IReviewed> iRevieweds;
+//
+//    public class ThemePark {
+//        this.attraction = new ArrayList<>();
+//        this.stalls = new ArrayList<>();
+//    }
+//
+//    public ArrayList<Attraction> getAttraction() {
+//        return attraction;
+//    }
+//
+//    public ArrayList<Stall> getStall(){
+//        return stalls;
+//    }
+
+
+}
